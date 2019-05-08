d9475ca7a61b7a77fc4d1fa7b7cda9a08e52b2ef
Huyen Pham <huyen.phamhoang96@gmail.com>
2019-05-08 16:59:40 +0700

    update

--- app/src/main/java/com/utildev/androidjetpack/MainActivity.kt
+++ app/src/main/java/com/utildev/androidjetpack/MainActivity.kt
@@ -2,11 +2,16 @@ package com.utildev.androidjetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val host = NavHostFragment.create(R.navigation.nav_graph)
+        supportFragmentManager.beginTransaction().replace(R.id.fmContainer, host)
+            .setPrimaryNavigationFragment(host)
+            .commit()
     }
 }
