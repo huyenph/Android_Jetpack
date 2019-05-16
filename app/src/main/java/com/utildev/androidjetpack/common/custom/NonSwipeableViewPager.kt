343458f0820a5023699cd4f3e3a20b961dd6d0f5
Huyen Pham <huyen.phamhoang96@gmail.com>
2019-05-16 10:42:14 +0700

    viewpager disable swiper

---
+++ app/src/main/java/com/utildev/androidjetpack/common/custom/NonSwipeableViewPager.kt
@@ -0,0 +1,45 @@
+package com.utildev.androidjetpack.common.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+
+class NonSwipeableViewPager : ViewPager {
+    constructor(context: Context) : super(context) {
+//        setMyScroller()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+//        setMyScroller()
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean {
+        // Never allow swiping to switch between pages
+        return false
+    }
+
+//    private fun setMyScroller() {
+//        try {
+//            val viewpager = ViewPager::class.java
+//            val scroller = viewpager.getDeclaredField("mScroller")
+//            scroller.isAccessible = true
+//            scroller.set(this, MyScroller(context))
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//    }
+//
+//    inner class MyScroller(context: Context) : Scroller(context, DecelerateInterpolator()) {
+//        override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+//            super.startScroll(startX, startY, dx, dy, 350 /*1 secs*/)
+//        }
+//    }
+}
